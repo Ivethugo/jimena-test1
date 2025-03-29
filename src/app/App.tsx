2f82a73f1612b28a1fde2e041bb3cbff79899b26
Hugo Barrera <hugcla@hotmail.com>
2025-03-29 02:28:31 -0600

    text1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    // Obtiene la cadena de consulta completa (incluye el signo '?')
+    const query = window.location.search;
+    console.log("Query String:", query);
+
+    // Convierte la cadena de consulta en un objeto más manejable
+    const params = new URLSearchParams(query);
+    const paramValue = params.get("nombre"); // Cambia 'miParametro' por el nombre del parámetro
+    console.log("Valor del parámetro:", paramValue);
+  }, []);
+
   return <>Hola hugo</>;
 }
 
